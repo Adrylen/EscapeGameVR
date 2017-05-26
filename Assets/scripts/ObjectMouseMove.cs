4d7b2921c1cf0683374bf68f081815e9498824bc
Louis-Quentin JOUCLA <louis-quentin.joucla@isen.yncrea.fr>
2017-05-26 11:38:03 +0200

    Ajout du script PlaySoundOnCollision et debut du script ObjectMouseMove

---
+++ Assets/scripts/ObjectMouseMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMouseMove : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float y = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(h, y, 0);
+	}
+}
---
+++ Assets/scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    public AudioSource sound;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+    }
+
+    // Mettre un collider sur chacun des objets
+    void OnCollisionEnter(Collision test)
+    {
+        if (test.gameObject.CompareTag("test"))
+        {
+            sound.Play();
+        }
+    }
+}
