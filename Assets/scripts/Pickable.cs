906ff2e2d4fda36f72dfcb7a1ffc8568bf5c3825
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-05-30 14:44:15 +0200

    Translations sur les objets

---
+++ Assets/scripts/Pickable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+	private SteamVR_TrackedController controller;
+	private GameObject target = null;
+	private Vector3 base_offset;
+	private Vector3 offset_position;
+
+	void OnEnable() {
+		controller = GetComponent<SteamVR_TrackedController>();
+	}
+
+	void Start() {
+		base_offset = new Vector3(-9999, -9999, -9999);
+	}
+
+	void Update() {
+		if (controller.triggerPressed) {
+			if(target != null) {
+				target.transform.position = transform.position + offset_position;
+			} else {
+				offset_position = base_offset;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pickable") && target == null) {
+			target = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag("Pickable") && target == other.gameObject) {
+			target = null;
+		}
+	}
+}
