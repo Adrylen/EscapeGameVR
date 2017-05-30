bf4c01aea56ccb6750c75788d87ac5ec3a39ee75
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-05-30 12:40:42 +0200

    Interaction VR Composant

--- Assets/scripts/Primitive.cs
+++ Assets/scripts/Primitive.cs
@@ -2,47 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Primitive : MonoBehaviour {
-	private SteamVR_TrackedController controller;
-	private GameObject target = null;
-	//private bool triggered;
+public class Primitive : MonoBehaviour
+{
+    private SteamVR_TrackedController controller;
+    private GameObject target = null;
+    private Vector3 base_offset;
+    private Vector3 offset_position;
+    private Quaternion offset_rotation;
 
-	public TextMesh text;
+    void OnEnable()
+    {
+        controller = GetComponent<SteamVR_TrackedController>();
+    }
 
-	private void OnEnable() {
-		controller = GetComponent<SteamVR_TrackedController> ();
-		controller.TriggerClicked += HandleTriggerClicked;
-		controller.PadClicked += HandlePadClicked;
-	}
+    void Start()
+    {
+        base_offset = new Vector3(-9999, -9999, -9999);
+    }
 
-	private void OnDisable() {
-		controller.TriggerClicked -= HandleTriggerClicked;
-		controller.PadClicked -= HandlePadClicked;
-	}
+    void Update()
+    {
+        if (controller.triggerPressed)
+        {
+            if(target != null) {
+                //offset_position = offset_position == base_offset ? target.transform.position : offset_position;
+                //offset_rotation = offset_rotation.eulerAngles == base_offset ? target.transform.rotation : offset_rotation;
+                target.transform.position = transform.position;// + offset_position;
+                target.transform.rotation = transform.rotation;// * offset_rotation;
+            } else {
+                offset_position = base_offset;
+                offset_rotation.eulerAngles = base_offset;
+            }
+        }
+    }
 
-	#region Enter Collider
-	void OnTriggerEnter(Collider other) {
-		if (target == null) {
-			target = other.gameObject;
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("In:" + other.name);
+        if (other.name=="Cube" && target == null)
+        {
+            target = other.gameObject;
+            //Debug.Log("In");
+        }
+    }
 
-	void OnTriggerExit(Collider other) {
-		if (target == other.gameObject) {
-			target = null;
-		}
-	}
-	#endregion
+    void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("Out:" + other.name);
 
-	#region Trigger
-	private void HandleTriggerClicked(object sender, ClickedEventArgs e) {
-		text.text = "Trigger";
-	}
-	#endregion
-
-	#region Pad
-	private void HandlePadClicked(object sender, ClickedEventArgs e) {
-		text.text = "Pad";
-	}
-	#endregion
+        if (other.name=="Cube" && target == other.gameObject)
+        {
+            target = null;
+            //Debug.Log("Out");
+        }
+    }
 }
