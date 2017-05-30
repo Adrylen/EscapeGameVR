aba69fcaebeebafd9394ce4e8bc614e317db13ad
christinelasquellec <christine.lasquellec@isen.yncrea.fr>
2017-05-30 08:53:21 +0200

    petite modif dans le script de la batterie

--- Assets/scripts/drum.cs
+++ Assets/scripts/drum.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class drum : MonoBehaviour {
 	public string keyCode;
+	private AudioSource son;
 
+	void Start() {
+		son = GetComponent<AudioSource> ();
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetKeyDown (keyCode)) {
-			GetComponent<AudioSource> ().Play ();
+			son.Play ();
 		}
 			
 	}
