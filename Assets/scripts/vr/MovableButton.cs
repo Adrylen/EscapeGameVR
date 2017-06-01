77b7b47bff418a5a9833a5ae8f96482f5d17f5f9
bloudman <adriencom@msn.com>
2017-06-01 09:44:52 +0200

    ajout test button

---
+++ Assets/scripts/vr/MovableButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableButton : Movable {
+	private Vector3 origin;
+	private bool active;
+
+	// Use this for initialization
+	void Start () {
+		active = false;
+		origin = transform.localPosition;
+	}
+
+	public override void Movement(GameObject controller) {
+		active = !active;
+		if (active) {
+			transform.localPosition = new Vector3 (origin.x, origin.y - 0.5f, origin.z);
+		} else {
+			transform.localPosition = new Vector3 (origin.x, origin.y, origin.z);
+		}					
+	}
+}
