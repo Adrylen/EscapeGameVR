0e1524b29c5fc1ab8a213d3025c05b02d26b9912
Louis-Quentin JOUCLA <louis-quentin.joucla@isen.yncrea.fr>
2017-06-08 17:41:50 +0200

    Generation/Supression grille boite à rythm codé, codeVR à faire

--- Assets/scripts/rythmTable/rythmTable.cs
+++ Assets/scripts/rythmTable/rythmTable.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rythmTable : MonoBehaviour {
+public class RythmTable : MonoBehaviour {
 
-    CustomArrayList test = new CustomArrayList();
+    private CustomArrayList test = new CustomArrayList();
     public GameObject templateCube;
-    Vector3 origin = new Vector3();
+    private Vector3 origin = new Vector3();
 
     // Use this for initialization
     void Start() {
         origin = transform.localPosition;
         InitRythmBox(test);
         addRows(test);
+        addLine(test);
     }
 
     // Update is called once per frame
     void Update() {
+
+        if (Input.GetMouseButton(0))
+        {
+            addLine(test);
+        }
+
         if (Input.GetMouseButton(1))
         {
-            addRows(test);
+            removeLine(test);
         }
     }
 
@@ -45,8 +52,8 @@ public class rythmTable : MonoBehaviour {
         aRythmBox.Add(newList2);
         aRythmBox.Add(newList3);
 
-        yPos = gameObject.transform.localPosition.y;
-        xPos = gameObject.transform.localPosition.x;
+        yPos = 0;
+        xPos = 0;
 
         // Cube filling
 
@@ -54,16 +61,18 @@ public class rythmTable : MonoBehaviour {
         {
             for (int i = 0; i < aRythmBox.nbElementPerLine; i++)
             {
-                tempPosition = new Vector3(xPos + transform.localScale.x * (1.1f * i), yPos, transform.localPosition.z);
+                tempPosition = new Vector3(xPos + (1.1f * i), yPos, 0);
                 element.Add((GameObject)Instantiate(templateCube, tempPosition, Quaternion.identity, temp.transform));
             }
-            yPos += transform.localScale.y * (1.1F);
+            yPos -= (1.1F);
             xPos = gameObject.transform.localPosition.x;
         }
 
         // Rotation adaptation
 
+        temp.transform.localScale = transform.localScale;
         temp.transform.rotation = transform.rotation;
+        temp.transform.localPosition = transform.position;
         temp.transform.parent = transform;
 
         // Moving children to root of the gameobject
@@ -85,26 +94,29 @@ public class rythmTable : MonoBehaviour {
     {
         float xPos, yPos;
         GameObject temp = new GameObject();
-        yPos = gameObject.transform.position.y;
+        yPos = 0;
 
         // Updating aRythmBox columnn number
       
-        xPos = gameObject.transform.position.x + transform.localScale.x * (1.1F) * aRythmBox.nbElementPerLine;
+        xPos = (1.1F) * aRythmBox.nbElementPerLine;
         aRythmBox.nbElementPerLine++;
 
         // Cube filling
 
         foreach (List<GameObject> element in aRythmBox)
         {
-            Vector3 tempPosition = new Vector3(xPos, yPos, transform.position.z);
+            Vector3 tempPosition = new Vector3(xPos, yPos, 0.0f);
             element.Add((GameObject)Instantiate(templateCube, tempPosition, Quaternion.identity, temp.transform));
-            yPos += transform.localScale.y * (1.1F);
+            yPos -= (1.1F);
         }
 
         // Rotation adaptation
 
+        temp.transform.localScale = transform.localScale;
         temp.transform.rotation = transform.rotation;
+        temp.transform.localPosition = transform.position;
         temp.transform.parent = transform;
+        
 
         // Moving children to root of the gameobject
 
@@ -118,5 +130,79 @@ public class rythmTable : MonoBehaviour {
 
         Destroy(temp);
 
-    }   
+    }
+
+    void addLine(CustomArrayList aRythmBox)
+    {
+        List<GameObject> newList = new List<GameObject>();
+        GameObject temp = new GameObject();
+        Vector3 tempPosition;
+        float yPos, xPos;
+        yPos = (-1.1F) * aRythmBox.nbLine;
+        xPos = 0;
+
+        aRythmBox.Add(newList);
+        aRythmBox.nbLine++;
+
+        for(int i=0; i<aRythmBox.nbElementPerLine;i++)
+        {
+            tempPosition = new Vector3(xPos + (1.1f * i), yPos, 0);
+            newList.Add((GameObject)Instantiate(templateCube, tempPosition, Quaternion.identity, temp.transform));
+        }
+
+        // Rotation adaptation
+
+        temp.transform.localScale = transform.localScale;
+        temp.transform.rotation = transform.rotation;
+        temp.transform.localPosition = transform.position;
+        temp.transform.parent = transform;
+
+
+        // Moving children to root of the gameobject
+
+        for (int i = temp.transform.childCount - 1; i >= 0; --i)
+        {
+            Transform child = temp.transform.GetChild(i);
+            child.transform.parent = gameObject.transform;
+        }
+
+        // Removing Object for Rotation
+
+        Destroy(temp);
+    }
+
+    void removeRow(CustomArrayList aRythmBox)
+    {
+        GameObject temp;
+
+        // removing last cube for each horizontal line
+        if(aRythmBox.nbElementPerLine > 1)
+        {
+            foreach (List<GameObject> aList in aRythmBox)
+            {
+                temp = aList[aList.Count - 1];
+                aList.RemoveAt(aList.Count - 1);
+                Destroy(temp);
+            }
+            // updating rows numbers
+            aRythmBox.nbElementPerLine--;
+        }
+
+    }
+
+    void removeLine(CustomArrayList aRythmBox)
+    {
+        if(aRythmBox.nbLine>1)
+        {
+            List<GameObject> tempList = (List<GameObject>)aRythmBox[aRythmBox.nbLine-1];
+
+            foreach (GameObject element in tempList)
+            {
+                Destroy(element);
+            }
+            tempList.Clear();
+            aRythmBox.RemoveAt(aRythmBox.nbLine - 1);
+            aRythmBox.nbLine--;
+        }
+    }
 }
