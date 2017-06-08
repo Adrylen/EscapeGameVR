52436fdadc885613839fc7c2741d0456ae4477ae
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-06-08 10:08:45 +0200

    Fix CrossFader

--- Assets/scripts/vr/MoveSlider.cs
+++ Assets/scripts/vr/MoveSlider.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class MoveSlider : Movable {
 	private Vector3 origin;
-	private float zMax = 0.5f;
-	private float zMin = -0.5f;
+	private float max = 0.5f;
+	private float min = -0.5f;
+
+	public bool xAxis = false;
 
 	void Start() {
 		origin = transform.localPosition;
 	}
-//
-//	void Update () {
-//		if (transform.localPosition != origin) {
-//			float zTemp = (transform.localPosition.z > zMax) ? zMax : (transform.localPosition.z < zMin) ? zMin : transform.localPosition.z;
-//			transform.localPosition = new Vector3 (origin.x, origin.y, zTemp);
-//		}
-//	}
-//
+
 	public override void Movement(GameObject controller) {
         transform.position = controller.transform.position;
 		if (transform.localPosition != origin) {
-			float zTemp = (transform.localPosition.z > zMax) ? zMax : (transform.localPosition.z < zMin) ? zMin : transform.localPosition.z;
-			transform.localPosition = new Vector3 (origin.x, origin.y, zTemp);
-            gameObject.GetComponentInParent<Effect>().ApplyEffect(zTemp+0.5f);
+			float newPosition = NewPosition ();
+			transform.localPosition = new Vector3 (origin.x, origin.y, newPosition);
+			gameObject.GetComponentInParent<Effect>().ApplyEffect(newPosition+0.5f);
         }
+	}
+
+	private float NewPosition() {
+		float localPosition = xAxis ? transform.localPosition.x : transform.localPosition.z;
+		return localPosition > max ? max : localPosition < min ? min : localPosition;
 	}
 }
