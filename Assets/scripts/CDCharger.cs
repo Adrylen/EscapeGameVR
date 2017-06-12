9996a3e0327caf395620717dc2aa45d2437b5867
bloudman <adriencom@msn.com>
2017-06-12 09:45:35 +0200

    chargement preload

--- Assets/scripts/CDCharger.cs
+++ Assets/scripts/CDCharger.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
 
 public class CDCharger : MonoBehaviour
 {
+
+	Dictionary<string, AudioClip> audioFiles;
+
 	public AudioSource sound;
     private string actualFileName = "";
 	// Use this for initialization
 	void Start()
 	{
+		audioFiles = new Dictionary<string, AudioClip>();
+
+		DirectoryInfo dir = new DirectoryInfo("assets/Resources");
+		FileInfo[] info = dir.GetFiles("*.mp3");
+
+		foreach (var file in info) {
+			string fileName;
+			fileName = Regex.Replace(file.Name, @".mp3", "");
+			audioFiles [fileName] = (AudioClip)Resources.Load (fileName);
+		}
+
 		GetComponent<AudioSource>().playOnAwake = false;
 		sound = GetComponent<AudioSource>();
 	}
@@ -20,15 +36,14 @@ public class CDCharger : MonoBehaviour
 
 		if (other.gameObject.CompareTag("Pickable"))
 		{
-            Debug.Log("collision");
 			if (other.GetComponent<CD> () != null && other. GetComponent<CD>().fileName!=actualFileName) {
 				CD cd = other.GetComponent<CD> ();
 
 				cd.gameObject.transform.parent = this.gameObject.transform;
 				cd.gameObject.transform.position = this.gameObject.transform.position;
-
+				cd.load ();
                 //AudioClip clip1 = (AudioClip)Resources.Load(other.GetComponent<CD>().fileName);
-				AudioClip clip1 = (AudioClip)Resources.Load(cd.fileName);
+				AudioClip clip1 = audioFiles[cd.fileName];
                 actualFileName = cd.fileName;
                 sound.Stop();
                 sound.PlayOneShot(clip1);
